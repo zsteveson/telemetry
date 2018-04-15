2401d65c7352cac1b8396b4e22e2e490ae49a539
Zachary <zsteveson@gmail.com>
2018-04-15 12:17:17 -0400

    Add log to troubleshoot JSON decoding

--- error.php
+++ error.php
@@ -2,11 +2,11 @@
 header('Access-Control-Allow-Origin: *');
 $data = file_get_contents("php://input");
 $error = json_decode($data,true);
+print_r($error);
 $errorLog = $error['message'] . ' ';
 $errorLog .= $error['url'] . ' ';
 $errorLog .= $error['line'] . ' '; 
 $errorLog .= $error['column'] . ' ';
 $errorLog .= $error['error'] . PHP_EOL;
 error_log($errorLog, 3, "/var/tmp/telemetryError.log");
-echo $errorLog;
 ?>
