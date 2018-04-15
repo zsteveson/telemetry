aa0054a87241de75c746266e9e35fcd3054b2177
Zachary <zsteveson@gmail.com>
2018-04-15 12:37:40 -0400

    Fix name

--- error.php
+++ error.php
@@ -3,6 +3,6 @@ header('Access-Control-Allow-Origin: *');
 $data = file_get_contents("php://input");
 $e = json_decode($data,true);
 $log = $e['message'] . $e['url'] . $e['line'] . ' ' . $e['column'] . $e['error'] . PHP_EOL;
-error_log($errorLog, 3, "/var/tmp/telemetryError.log");
-echo $errorLog;
+error_log($log, 3, "/var/tmp/telemetry.log");
+echo $log
 ?>
