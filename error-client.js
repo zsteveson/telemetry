b0c06e939b81bf27a069d12648d5cfbadf77e37e
Zachary <zsteveson@gmail.com>
2018-04-15 12:36:41 -0400

    change things

--- error-client.js
+++ error-client.js
@@ -5,7 +5,7 @@ window.onerror = function (msg, url, lineNo, columnNo, error) {
 
   xhr.open('POST', 'http://ec2-54-236-247-79.compute-1.amazonaws.com/telemetry/error.php',true);
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(log);
+  xhr.send(JSON.stringify(log));
   
  }
 
