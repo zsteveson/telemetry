e74032361bbbd0bf8f2a43e5b9bcd955bf0fa6ae
Zachary <zsteveson@gmail.com>
2018-04-15 12:29:57 -0400

    blah

--- error.php
+++ error.php
@@ -3,10 +3,12 @@ header('Access-Control-Allow-Origin: *');
 $data = file_get_contents("php://input");
 $error = json_decode($data);
 print_r($error);
+$error = json_decode($data,true);
 $errorLog = $error['message'] . ' ';
 $errorLog .= $error['url'] . ' ';
 $errorLog .= $error['line'] . ' '; 
 $errorLog .= $error['column'] . ' ';
 $errorLog .= $error['error'] . PHP_EOL;
 error_log($errorLog, 3, "/var/tmp/telemetryError.log");
+echo $errorLog;
 ?>
