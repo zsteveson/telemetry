1c27434ff64f46744d3f28f93f786d456507eabc
Zachary <zsteveson@gmail.com>
2018-04-15 13:23:12 -0400

    Changes

--- error-client.js
+++ error-client.js
@@ -1,13 +1,11 @@
 window.onerror = function (msg, url, lineNo, columnNo, error) {
-
-  var log = { 'message' : msg, 'url' : url, 'line' : lineNo, 'column' : columnNo, 'error'  : error }     
+  console.log(error);
+  console.log(JSON.stringify(error));
+  var log = { 'message' : msg, 'url' : url, 'line' : lineNo, 'column' : columnNo }     
   var xhr = new XMLHttpRequest();
 
   xhr.open('POST', 'http://ec2-54-236-247-79.compute-1.amazonaws.com/telemetry/error.php',true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify(log));
-  
+
  }
-
-
-
--- error.php
+++ error.php
@@ -2,9 +2,12 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
+
 $data = file_get_contents("php://input");
 $e = json_decode($data,true);
-$log = $e['message'] . $e['url'] . $e['line'] . ' ' . $e['column'] . $e['error'] . PHP_EOL;
+
+$log = $e['message'] . ' ' . $e['url'] . ' ' . $e['line'] . ' ' . $e['column'] . PHP_EOL;
 error_log($log, 3, "/var/tmp/telemetry.log");
 echo $log
 ?>
+
