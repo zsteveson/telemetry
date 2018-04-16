f36c8e5308894305bb791dfb4561e22979d197d1
Zachary <zsteveson@gmail.com>
2018-04-16 12:50:32 -0400

    Consistent variable naming for error

--- error.php
+++ error.php
@@ -6,11 +6,11 @@ header('Access-Control-Allow-Headers: Content-Type');
 $data = file_get_contents("php://input");
 logJavasScriptError(json_decode($data,true));
 
-function logJavasScriptError($e) {
+function logJavasScriptError($error) {
 	error_log(formatError($e), 3, "/var/tmp/telemetry.log");
 }
 
 function formatError($error) {
-	return $e['message'] . ' ' . $e['url'] . ' ' . $e['line'] . ' ' . $e['column'] . PHP_EOL;
+	return $error['message'] . ' ' . $error['url'] . ' ' . $error['line'] . ' ' . $error['column'] . PHP_EOL;
 }
 ?>
