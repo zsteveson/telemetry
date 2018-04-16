a50996e08fdef5b1bf9717f43ccb1d9973e96aef
Zachary <zsteveson@gmail.com>
2018-04-15 21:38:48 -0400

    Fire performance after load

--- performance-client.js
+++ performance-client.js
@@ -1,3 +1,5 @@
+window.addEventListener("load", function(){
+
 var log = {'navigation' : window.performance.getEntriesByType('navigation'),
              'resource' : window.performance.getEntriesByType('resource') };
 
@@ -6,3 +8,5 @@ var xhr = new XMLHttpRequest();
 xhr.open('POST', 'http://ec2-54-236-247-79.compute-1.amazonaws.com/telemetry/performance.php',true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify(log));
+
+}
