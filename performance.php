4e521c26a7c03c4d78bc8eabfa261f96952c4398
Zachary <zsteveson@gmail.com>
2018-04-26 00:15:30 -0400

    ?

--- performance.php
+++ performance.php
@@ -6,22 +6,19 @@ header('Access-Control-Allow-Headers: Content-Type');
 $data = file_get_contents("php://input");
 $data = json_decode($data,true);
 
-logNavigation($data['location'], $data['navigation']);
-
-logResources($data['location'], $data['resource']);
-
-
+logNavigation($data);
+logResources($data);
 
 function logNavigation($data) {
   $timing = $data['navigation'];
   $log = Array( 
-  	'dns'   => $timing['domainLookupEnd'] - $timing['domainLookupStart'],
-  	'tcp'   => $timing['connectEnd'] - $timing['connectStart'],
-  	'ttfb'	=> $timing['responseStart'] - $timing['requestStart'],
-  	'transfer' => $timing['responseEnd'] - $timing['responseStart'],
-  	'dominteractive' => $timing['domInteractive'] - $timing['domLoading'],
-  	'domcomplete' => $timing['domComplete'] - $timing['domInteractive'],
-  	'totalpageloadtime' =>$timing['loadEventEnd'] - $timing['navigationStart'],
+    'dns'   => $timing['domainLookupEnd'] - $timing['domainLookupStart'],
+    'tcp'   => $timing['connectEnd'] - $timing['connectStart'],
+    'ttfb'  => $timing['responseStart'] - $timing['requestStart'],
+    'transfer' => $timing['responseEnd'] - $timing['responseStart'],
+    'dominteractive' => $timing['domInteractive'] - $timing['domLoading'],
+    'domcomplete' => $timing['domComplete'] - $timing['domInteractive'],
+    'totalpageloadtime' =>$timing['loadEventEnd'] - $timing['navigationStart'],
     'location' => $data['location']
   );
   error_log(json_encode($log) . PHP_EOL, 3, "/var/tmp/pageperformance.log");
